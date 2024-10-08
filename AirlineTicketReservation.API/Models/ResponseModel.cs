e094bb4ddd29e2a0c6b39baa2992add9f509bd57
igormivanov <igormivanovx@gmail.com>
2024-10-08 12:22:12 -0300

    feat: adicionar classe ResponseModel para padronizar respostas dos endpoints

---
+++ AirlineTicketReservation.API/Models/ResponseModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace AirlineTicketReservation.API.Models {
+    public class ResponseModel<T> {
+
+        [JsonProperty("messages", NullValueHandling = NullValueHandling.Include)]
+        public List<string> Messages { get; set; } = new List<string>();
+
+        [JsonProperty("results", NullValueHandling = NullValueHandling.Include)]
+        public List<T> Results { get; set; } = new List<T>();
+        public bool Status { get; set; } = true;
+
+        public ResponseModel() {}
+
+        public ResponseModel(IEnumerable<T> body) {
+            Results.AddRange(body);
+        }
+
+        public ResponseModel(T body) : this() {
+            Results.Add(body);
+        }
+
+        
+    }
+}
